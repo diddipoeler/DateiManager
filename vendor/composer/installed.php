89fafcfc7b39523f651079d23b357167d3f47aeb
diddipoeler <diddipoeler@gmx.de>
2026-07-03 13:52:09 +0200

    neu

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'diddipoeler/dateimanager',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6f9c10186b791030cfc0d6fb6e10ecb52ad1eedb',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'diddipoeler/dateimanager' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6f9c10186b791030cfc0d6fb6e10ecb52ad1eedb',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
